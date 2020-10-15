49979e5f47c8da6faf0e11cdbd773e48b81cf1cf
Jamal Pelpuo <jpelpuo@ymail.com>
2020-10-15 18:39:22 +0000

    Clean up

--- services/course/getCourses.js
+++ services/course/getCourses.js
@@ -1,12 +1,15 @@
 const Course = require('../../dBmodels/course');
 const createError = require('http-errors');
 const Admin = require('../../dBmodels/admin');
+const User = require('../../dBmodels/user')
 
 module.exports = getCourses = async (userId) => {
     try {
         const admin = await Admin.findById(userId);
 
-        if (!admin) {
+        const user = await User.findById(userId);
+
+        if (!admin && !user) {
             throw createError.Unauthorized()
         }
 

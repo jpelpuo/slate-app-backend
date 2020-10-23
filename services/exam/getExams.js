bffa3396db83e2b8ca37d207c39b47575ccc9f27
Jamal Pelpuo <jpelpuo@ymail.com>
2020-10-23 00:10:26 +0000

    Adds get exams route

--- services/exam/getExams.js
+++ services/exam/getExams.js
@@ -6,7 +6,7 @@ module.exports = getExams = async () => {
     try {
         const exams = await Exam.find().populate('questions').exec()
 
-        return exams._doc
+        return exams
 
     } catch (error) {
         throw error
